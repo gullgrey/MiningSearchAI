4defbe4746be4a97cb5100b5dd602dbae71cd35f
Solpalace <Navyge243!>
2021-04-06 13:06:10 +1000

    Version 3
    Working on is_dangerous

--- tests.py
+++ tests.py
@@ -3,12 +3,23 @@ from search import *
 import numpy as np
 
 
+x = np.array([[[1, 2], [1, 2], [1, 2]],
+                  [[1, 2], [1, 2], [1, 2]],
+                  [[1, 2], [1, 2], [1, 2]],
+                  [[1, 2], [1, 2], [1, 2]]])
+
+y = [[1,2],[3,4],[5,6]]
+
 def test_1():
-    mine = Mine(np.array([[1,2],[3,4]]))
+
+    mine = Mine(np.array(x))
     print(mine)
+    print(mine.cumsum_mine)
+    print(mine.initial)
 
 
 test_1()
+
 
 def test_dig_tol():
     mine = Mine(np.array([[1,2],[3,4]]))
