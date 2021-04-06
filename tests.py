a9e4211e6befe91ac557f436cd64fbd6d8a90b51
gullgrey <thomas.cleal@connect.qut.edu.au>
2021-04-06 12:57:41 +1000

    Version 3
    Updated Mine.__init__

--- tests.py
+++ tests.py
@@ -2,10 +2,20 @@ from mining import *
 from search import *
 import numpy as np
 
+x = np.array([[[1, 2], [1, 2], [1, 2]],
+                  [[1, 2], [1, 2], [1, 2]],
+                  [[1, 2], [1, 2], [1, 2]],
+                  [[1, 2], [1, 2], [1, 2]]])
+
+y = [[1,2],[3,4],[5,6]]
 
 def test_1():
-    mine = Mine(np.array([[1,2],[3,4]]))
+
+    mine = Mine(np.array(x))
     print(mine)
+    print(mine.cumsum_mine)
+    print(mine.initial)
 
 
 test_1()
+# print(x.cumsum(axis = 2))
