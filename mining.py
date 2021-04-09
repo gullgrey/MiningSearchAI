a8a2bd5ee9d3354984f4df964314c8923a508781
clancyhaupt <clancy.haupt@connect.qut.edu.au>
2021-04-09 12:11:43 +1000

    update student ID

--- mining.py
+++ mining.py
@@ -56,7 +56,7 @@ def my_team():
 
     '''
     # TODO add everyone's name and student number
-    return [(10895159, 'Thomas', 'Cleal'), (10583084, 'Michael', 'Solomon'), ()]
+    return [(10895159, 'Thomas', 'Cleal'), (10583084, 'Michael', 'Solomon'), (10154337, Clancy, Haupt)]
 
 
 def convert_to_tuple(a):
